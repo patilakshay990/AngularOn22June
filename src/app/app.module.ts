2c6f301986e7c8ef39525044d5714e5ccef6c9ee
Akshay Patil <patilakshay1934@gmail.com>
2019-06-24 00:14:52 -0700

    create new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { ComponentNewComponent } from './component-new/component-new.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    ComponentNewComponent
   ],
   imports: [
     BrowserModule,
